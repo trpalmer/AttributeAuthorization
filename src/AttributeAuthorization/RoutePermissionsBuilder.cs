5e32f8eca7b6cdf8ec5a06e120fa3d8884623436
AD\Lcastleberry <lcastleberry@trainingpeaks.com>
2018-03-26 09:13:41 -0600

    Adjustments for WebApi2

--- src/AttributeAuthorization/RoutePermissionsBuilder.cs
+++ src/AttributeAuthorization/RoutePermissionsBuilder.cs
@@ -4,29 +4,13 @@ using System.Linq;
 using System.Net.Http;
 using System.Reflection;
 using System.Web.Http;
-using System.Web.Http.Controllers;
-using System.Web.Http.Dispatcher;
 using System.Web.Http.Routing;
 
 namespace AttributeAuthorization
 {
     public class RoutePermissionsBuilder
     {
-        internal class RouteInfo
-        {
-            public RouteInfo(string controllerName, string actionName)
-            {
-                ControllerName = controllerName;
-                ActionName = actionName;
-            }
-
-            public string ControllerName { get; private set; }
-            public string ActionName { get; private set; }
-        }
-
         private readonly HttpConfiguration _configuration;
-        private IHttpControllerSelector _controllerSelector;
-        private Action<IHttpRoute, Dictionary<string, AuthPermissions>> _undefinedRoute;
 
         // consider: Methods that are decorated with RequiresAuth AND RequiresNoAuth. Current just falls back to not allowed
         // Add a callback on build, include a callback that would throw an exception on build?
@@ -34,79 +18,39 @@ namespace AttributeAuthorization
         {
             configuration.CheckNull("configuration");
             _configuration = configuration;
-            _undefinedRoute = undefinedRouteAction;
         }
 
         public Dictionary<string, AuthPermissions> Build()
         {
-            InitializeFromConfiguration();
             return BuildPermissions();
-        }
-
-        private void InitializeFromConfiguration()
-        {
-            _controllerSelector = _configuration.Services.GetService(typeof(IHttpControllerSelector)) as IHttpControllerSelector;
-            if (_controllerSelector == null)
-            {
-                throw new NullReferenceException(String.Format("Could not locate IHttpControllerSelector in configuration"));
-            }
         }
 
         private Dictionary<string, AuthPermissions> BuildPermissions()
         {
             var map = new Dictionary<string, AuthPermissions>();
-            foreach (var route in _configuration.Routes)
-            {
-                RouteInfo info;
-                HttpControllerDescriptor controllerDescriptor;
-                if (ExtractRouteInfo(route, out info) && FindControllerDescriptor(info, out controllerDescriptor))
+	        var apiDescriptions = _configuration.Services.GetApiExplorer().ApiDescriptions;
+			foreach (var description in apiDescriptions)
+			{
+				var actionDescriptor = description.ActionDescriptor;
+				var controllerDescriptor = actionDescriptor.ControllerDescriptor;
+                var mapAdditions = new Dictionary<string, AuthPermissions>();
+                bool isPublic = actionDescriptor.GetCustomAttributes<RequiresNoAuth>().Any() ||
+                                controllerDescriptor.GetCustomAttributes<RequiresNoAuth>().Any();
+
+                var accepted = actionDescriptor.GetCustomAttributes<RequiresAuth>().SelectMany(a => a.Expand())
+                    .Concat(controllerDescriptor.GetCustomAttributes<RequiresAuth>().SelectMany(a => a.Expand()))
+                    .Distinct().ToList();
+
+                mapAdditions = GetMapForRoute(description.Route, isPublic, accepted);
+                foreach (var addMap in mapAdditions)
                 {
-                    var mapAdditions = new Dictionary<string, AuthPermissions>();
-                    var method = controllerDescriptor.ControllerType.GetMethod(info.ActionName);
-
-                    bool isPublic = method.GetCustomAttributes<RequiresNoAuth>().Any() || controllerDescriptor.GetCustomAttributes<RequiresNoAuth>().Any();
-
-                    var accepted = method.GetCustomAttributes<RequiresAuth>().SelectMany(a => a.Expand())
-                        .Concat(controllerDescriptor.GetCustomAttributes<RequiresAuth>().SelectMany(a => a.Expand()))
-                        .Distinct().ToList();
-
-                    mapAdditions = GetMapForRoute(route, isPublic, accepted);
-                    foreach (var addMap in mapAdditions)
+                    if (!map.ContainsKey(addMap.Key))
                     {
-                        if (!map.ContainsKey(addMap.Key))
-                        {
-                            map.Add(addMap.Key, addMap.Value);
-                        }
-                    }
-                }
-                else
-                {
-                    if (_undefinedRoute != null)
-                    {
-                        _undefinedRoute(route, map);
+                        map.Add(addMap.Key, addMap.Value);
                     }
                 }
             }
             return map;
-        }
-
-        private bool ExtractRouteInfo(IHttpRoute route, out RouteInfo info)
-        {
-            bool result = false;
-            info = null;
-            if (route.Defaults.ContainsKey("controller") && route.Defaults.ContainsKey("action"))
-            {
-                info = new RouteInfo(route.Defaults["controller"].ToString(), route.Defaults["action"].ToString());
-                result = true;
-            }
-            return result;
-        }
-
-        private bool FindControllerDescriptor(RouteInfo info, out HttpControllerDescriptor descriptor)
-        {
-            descriptor = _controllerSelector.GetControllerMapping()
-                .FirstOrDefault(s => String.Compare(s.Key, info.ControllerName, StringComparison.InvariantCultureIgnoreCase) == 0).Value;
-            return descriptor != null;
         }
 
         private Dictionary<string, AuthPermissions> GetMapForRoute(IHttpRoute route, bool isPublic, List<string> accepted)
@@ -172,7 +116,6 @@ namespace AttributeAuthorization
                     }
                 }
             }
-
             return constraints;
         }
     }
